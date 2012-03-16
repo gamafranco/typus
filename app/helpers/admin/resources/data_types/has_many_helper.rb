8c9754934a70d787aa632cc614b53e8936d7b13f
tfranco <tfranco@imaginarycloud.com>
2012-03-16 23:46:39 +0000

    Removes pop-up from the has_many show page.

--- app/helpers/admin/resources/data_types/has_many_helper.rb
+++ app/helpers/admin/resources/data_types/has_many_helper.rb
@@ -43,7 +43,7 @@ module Admin::Resources::DataTypes::HasManyHelper
 
   def set_has_many_resource_actions
     @resource_actions = [["Edit", { :action => "edit", :_popup => true }, { :class => 'iframe_with_page_reload' }],
-                         ["Show", { :action => "show", :_popup => true }, { :class => 'iframe'}],
+                         ["Show", { :action => "show" }, { :class => 'iframe'}],
                          ["Trash", { :action => "destroy" }, { :confirm => "Trash?" } ]]
   end
 
